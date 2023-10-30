f6d8fed575536da63cf3bab34e70baa27e866ee5
felixjosquin <felix.josquin@depinfonancy.net>
2023-10-30 22:28:58 +0100

    change draw

--- draw.py
+++ draw.py
@@ -2,25 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from simulation import l_bar, dt
+import time
+
+fps_anim = 18
 
 
 class Draw:
-    def __init__(self, X_historique):
-        self.x = X_historique[0, :]
-        self.dx = X_historique[1, :]
-        self.theta = X_historique[2, :]
-        self.dtheta = X_historique[3, :]
-        self.time = X_historique[4, :]
-        self.d2x = X_historique[5, :]
+    def __init__(self, X):
+        self.theta = X[:, 0]
+        self.x = X[:, 2]
+        self.time = np.array([i * dt for i in range(len(X))])
         self.run_simu = True
 
     def draw_cart(self, i=0):
         if self.run_simu:
+            a = int(1 / (fps_anim * dt))
             plt.clf()
-            plot_cart(self.x[i], self.theta[i], self.time[i], self)
-            plt.pause(dt / 10)
-            if i + 1 < len(self.time):
-                self.draw_cart(i + 1)
+            self.plot_cart(i)
+            pause = dt * a - 0.02 if dt * a > 0.02 else 0.001  # supp calcul time
+            plt.pause(pause)
+            i += a
+            if i < len(self.time):
+                self.draw_cart(i)
 
     def draw_graph(self):
         fig = plt.figure()
@@ -54,53 +57,53 @@ class Draw:
         )
         plt.show()
 
+    def plot_cart(self, i):
+        xt, theta, time = self.x[i], self.theta[i], self.time[i]
 
-def plot_cart(xt, theta, time, draw):
-    cart_w = 1.0  # longeur du card
-    cart_h = 0.5  # hauteur du card
-    radius = 0.1
+        cart_w = 1.0  # longeur du card
+        cart_h = 0.5  # hauteur du card
+        radius = 0.1
 
-    plt.xlim([-2.0, 2.0])
-    # Cordonée des point de la card
-    cx = np.array(
-        [-cart_w / 2.0, cart_w / 2.0, cart_w / 2.0, -cart_w / 2.0, -cart_w / 2.0]
-    )
-    cy = np.array([0.0, 0.0, cart_h, cart_h, 0.0])
-    cy += radius * 2.0
+        cx = np.array(
+            [-cart_w / 2.0, cart_w / 2.0, cart_w / 2.0, -cart_w / 2.0, -cart_w / 2.0]
+        )
+        cy = np.array([0.0, 0.0, cart_h, cart_h, 0.0])
+        cy += radius * 2.0
 
-    cx = cx + xt
+        cx = cx + xt
 
-    bx = np.array([0.0, l_bar * math.sin(-theta)])
-    bx += xt
-    by = np.array([cart_h, l_bar * math.cos(-theta) + cart_h])
-    by += radius * 2.0
+        bx = np.array([0.0, l_bar * math.sin(-theta)])
+        bx += xt
+        by = np.array([cart_h, l_bar * math.cos(-theta) + cart_h])
+        by += radius * 2.0
 
-    angles = np.arange(0.0, math.pi * 2.0, math.radians(3.0))
-    ox = np.array([radius * math.cos(a) for a in angles])
-    oy = np.array([radius * math.sin(a) for a in angles])
+        angles = np.arange(0.0, math.pi * 2.0, math.radians(3.0))
+        ox = np.array([radius * math.cos(a) for a in angles])
+        oy = np.array([radius * math.sin(a) for a in angles])
 
-    rwx = np.copy(ox) + cart_w / 4.0 + xt
-    rwy = np.copy(oy) + radius
-    lwx = np.copy(ox) - cart_w / 4.0 + xt
-    lwy = np.copy(oy) + radius
+        rwx = np.copy(ox) + cart_w / 4.0 + xt
+        rwy = np.copy(oy) + radius
+        lwx = np.copy(ox) - cart_w / 4.0 + xt
+        lwy = np.copy(oy) + radius
 
-    wx = np.copy(ox) + bx[-1]
-    wy = np.copy(oy) + by[-1]
+        wx = np.copy(ox) + bx[-1]
+        wy = np.copy(oy) + by[-1]
 
-    plt.plot(cx, cy, "-b")
-    plt.plot(bx, by, "-k")
-    plt.plot(rwx, rwy, "-k")
-    plt.plot(lwx, lwy, "-k")
-    plt.plot(wx, wy, "-k")
-    plt.title(f"x: {xt:.2f} , theta: {math.degrees(theta):.2f}, time: {time:.2f}")
+        plt.plot(cx, cy, "-b")
+        plt.plot(bx, by, "-k")
+        plt.plot(rwx, rwy, "-k")
+        plt.plot(lwx, lwy, "-k")
+        plt.plot(wx, wy, "-k")
+        plt.title(f"x: {xt:.2f} , theta: {math.degrees(theta):.2f}, time: {time:.2f}")
 
-    # for stopping simulation with the esc key.
-    plt.gcf().canvas.mpl_connect(
-        "key_release_event",
-        lambda event: [on_escape(draw, event) if event.key == "escape" else None],
-    )
-    plt.axis("equal")
+        # for stopping simulation with the esc key.
+        plt.gcf().canvas.mpl_connect(
+            "key_release_event",
+            lambda event: [self.on_escape() if event.key == "escape" else None],
+        )
 
+        plt.ylim(cart_h - l_bar - 3 * radius - 0.1, cart_h + l_bar + 3 * radius + 0.1)
+        plt.xlim(xt - l_bar - 3 * radius - 0.1, xt + l_bar + 3 * radius + 0.1)
 
-def on_escape(draw, event):
-    draw.run_simu = False
+    def on_escape(self):
+        self.run_simu = False
